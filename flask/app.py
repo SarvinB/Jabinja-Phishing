7111cfc6ae647a2abf99a0be1f4879e3e00bc73b
Sarvin Baghi <sarvin80.b@gmail.com>
2026-05-17 00:48:01 +0330

    fulogin url updates + solving error of refreshing login page

--- flask/app.py
+++ flask/app.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 session = requests.Session()
+login_check = False
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36",
     "Accept-Language": "en-US,en;q=0.9",
@@ -17,10 +18,6 @@ headers = {
 }
 data = {}
 
-# your custom logic that generates next HTML content
-# render_template("https%3A%2F%2Fjobinja.ir.html")
-# https%3A%2F%2Fjobinja.ir%2Fcv-builder
-
 @app.route("/", methods=["GET"])
 def login_page():
     return render_template("login.html")
@@ -29,48 +26,48 @@ def login_page():
 @app.route("/login", methods=["POST"])
 def login():
     
-    login_url= "https://jobinja.ir/login/user"
-    username = request.form.get("identifier")
-    password = request.form.get("password")
-
-    r = session.get(login_url, headers=headers)
-
-    soup = BeautifulSoup(r.text, "html.parser")
-
-    csrf = soup.find("input", {"name": "_token"})["value"]
-
-    payload = {
-        "identifier": username,
-        "password": password,
-        "_token": csrf
-    }
-
-    # headers = {
-    #     "Referer": login_url,
-    #     # "X-Requested-With": "XMLHttpRequest",
-    #     "User-Agent": "Mozilla/5.0"
-    # }
-
-    response = session.post(login_url, data=payload, headers=headers)
+    global login_check
+    global data
     
-    if "ورود" in response.text:
-        
-        # print(response.status_code)
-        # print(response.url)
-        # print(response.headers)
-        # print(username, password)
-        
-        # html = get_html("https://jobinja.ir")
-        # with open("templates/home.html", "w", encoding="utf-8") as f:
-        #     f.write(html)
-        # return Response(html, mimetype="text/html")
-
-        print("*********", response.status_code)
-        return render_template("login.html") 
+    if login_check is False:
     
+        login_url= "https://jobinja.ir/login/user"
+        username = request.form.get("identifier")
+        password = request.form.get("password")
+
+        r = session.get(login_url, headers=headers)
+
+        soup = BeautifulSoup(r.text, "html.parser")
+
+        csrf = soup.find("input", {"name": "_token"})["value"]
+
+        payload = {
+            "identifier": username,
+            "password": password,
+            "_token": csrf
+        }
+        
+        response = session.post(login_url, data=payload, headers=headers)
+        
+        if "ورود" in response.text:
+
+            print("*********", response.status_code)
+            return render_template("login.html") 
+        
+        else:
+            
+            login_check = True
+            
+            data = get_jobs_applied()
+            
+            with open("jobs_applied.json", "w", encoding="utf-8") as f:
+                json.dump(data, f, ensure_ascii=False, indent=2)
+            
+            html = get_html("https://jobinja.ir/", "templates/home.html")
+                
+            return Response(html, mimetype="text/html") 
     else:
         
-        global data
         data = get_jobs_applied()
         
         with open("jobs_applied.json", "w", encoding="utf-8") as f:
@@ -78,10 +75,6 @@ def login():
         
         html = get_html("https://jobinja.ir/", "templates/home.html")
         
-
-        # with open("templates/home.html", "r", encoding="utf-8") as f:
-        #     html = f.read()
-            
         return Response(html, mimetype="text/html") 
     
 @app.route("/jobs/applied/<job_id>/details")
@@ -91,7 +84,6 @@ def job_detail(job_id):
 @app.route("/<path:any_path>")
 def catch_all(any_path):
     
-    # html = get_html("https://jobinja.ir/jobs/applied")
     try:
         html = get_html("https://jobinja.ir/" + any_path)
         return Response(html, mimetype="text/html") 
@@ -104,9 +96,6 @@ def catch_all(any_path):
 def get_html(url, name="page.html"):
 
     r = session.get(url, headers=headers)
-    
-    # print(r.text)
-    
 
     replacements = {
         "https://jobinja.ir":
@@ -132,22 +121,6 @@ def get_html(url, name="page.html"):
 
     for old, new in replacements.items():
         html = html.replace(old, new)
-    
-    # soup = BeautifulSoup(r.text, "html.parser")
-    
-    # for tag in soup.find_all("a", href=True):
-        
-    #     if "https://jobinja.ir/assets/css/end_user" in tag["href"]:
-    #         pass
-        
-    #     elif "jobinja.ir" in tag["href"]:
-    #         tag["href"] = tag["href"].replace(
-    #             "https://jobinja.ir/",
-    #             "http://127.0.0.1:5000/"
-    #         )
-    
-    # with open(name, "w", encoding="utf-8") as f:
-    #     f.write(html)
         
     return html
 
@@ -157,21 +130,6 @@ def get_jobs_applied():
     r = session.get(url, headers=headers)
     
     return r.json()
-
-    # print(data["applications"]["data"][0]["short_id"])
-    
-# def get_job_details_url():
-
-#     applications = []
-    
-#     for job in data["applications"]["data"]:
-        
-#         dict = {"short_id": job["short_id"], "details_link": job["details_link"]}
-        
-#         applications.append(job["details_link"])
-    
-#     # applications = json.dumps(applications)
-#     return str(applications)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
